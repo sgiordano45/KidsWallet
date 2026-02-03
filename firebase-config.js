c96129d6f590705db42e5cc8dca7a332ee979730
Stephen Giordano <notredamegeo@gmail.com>
2026-02-03 09:11:16 -0500

    Move Firebase config to separate file

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAavO4IE21FEwBeY7fuFdBFYc9dflpLk90",
+  authDomain: "kidswallet-406a6.firebaseapp.com",
+  projectId: "kidswallet-406a6",
+  storageBucket: "kidswallet-406a6.firebasestorage.app",
+  messagingSenderId: "367658453381",
+  appId: "1:367658453381:web:790006549937f9f7fc5cec"
+};
